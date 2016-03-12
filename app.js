fc4d39866afce3e96eb9c0b29456ef4429895645
Obie Reynolds <obiereynolds@gmail.com>
2016-03-12 13:10:28 -0800

    committing from laptop on 03/12 - used Express.js to add a bunch of web service stuff

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
